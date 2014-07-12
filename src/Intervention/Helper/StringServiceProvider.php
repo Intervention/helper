de5542abfab53456783ae80f89858a515a36cf3b
Oliver Vogel <oliver@olivervogel.net>
2014-07-12 10:07:40 +0200

    switched loading of serviceprovider to deferred

--- src/Intervention/Helper/StringServiceProvider.php
+++ src/Intervention/Helper/StringServiceProvider.php
@@ -11,7 +11,7 @@ class StringServiceProvider extends ServiceProvider
      *
      * @var bool
      */
-    protected $defer = false;
+    protected $defer = true;
 
     /**
      * Bootstrap the application events.
